3a350b97a4eec1c79e629fdc8c289a41fe0558e9
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-19 17:13:27 +0200

    created db.php containing associative arrays for site links

---
+++ config/db.php
@@ -0,0 +1,173 @@
+<?php
+    return [
+        'navbarLinks' => [
+            [
+                'text' => 'Digital comics',
+                'image' => 'img/buy-comics-digital-comics.png'
+            ],
+            [
+                'text' => 'DC Merchandise',
+                'image' => 'img/buy-comics-merchandise.png'
+            ],
+            [
+                'text' => 'Subscription',
+                'image' => 'img/buy-comics-subscriptions.png'
+            ],
+            [
+                'text' => 'Comics Shop Locator',
+                'image' => 'img/buy-comics-shop-locator.png'
+            ],
+            [
+                'text' => 'DC Power Visa',
+                'image' => 'img/buy-dc-power-visa.svg'
+            ]
+        ],
+        'dcComicsLinks' => [
+            [
+                'text' => 'Characters',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Comics',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Movies',
+                'link' => '#'
+            ],
+            [
+                'text' => 'TV',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Games',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Collectibles',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Videos',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Fans',
+                'link' => '#'
+            ],
+            [
+                'text' => 'News',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Shop',
+                'link' => '#'
+            ]
+        ],
+        'shopLinks' => [
+            [
+                'text' => 'Shop DC',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Shop DC Collectibles',
+                'link' => '#'
+            ]
+        ],
+        'dcLinks' => [
+            [
+                'text' => 'Terms Of Use',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Privacy policy (New)',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Ad Choices',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Advertising',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Jobs',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Subscriptions',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Talent Workshops',
+                'link' => '#'
+            ],
+            [
+                'text' => 'CPSC Certificates',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Ratings',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Shop Help',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Contact Us',
+                'link' => '#'
+            ]
+        ],
+        'sitesLinks' => [
+            [
+                'text' => 'DC',
+                'link' => '#'
+            ],
+            [
+                'text' => 'MAD Magazine',
+                'link' => '#'
+            ],
+            [
+                'text' => 'DC Kids',
+                'link' => '#'
+            ],
+            [
+                'text' => 'DC Universe',
+                'link' => '#'
+            ],
+            [
+                'text' => 'DC Power Visa',
+                'link' => '#'
+            ],
+        ],
+        'socials' => [
+            [
+                'text' => 'Facebook',
+                'image' => 'img/footer-facebook.png',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Twitter',
+                'image' => 'img/footer-twitter.png',
+                'link' => '#'
+            ],
+            [
+                'text' => 'YouTube',
+                'image' => 'img/footer-youtube.png',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Pinterest',
+                'image' => 'img/footer-pinterest.png',
+                'link' => '#'
+            ],
+            [
+                'text' => 'Periscope',
+                'image' => 'img/footer-periscope.png',
+                'link' => '#'
+            ],
+        ]
+    ]
+?>
